bfcddfc150d43bb281af56e640f480279c199e4c
Hendry <ruckshan@Ruckshans-MacBook-Pro.local>
2019-09-30 23:09:37 -0400

    fixed if price is free

--- scrapper.py
+++ scrapper.py
@@ -29,10 +29,10 @@ def search(key):
         shops.append(pageShops[0].img['alt'])
 
         # This should fix issue when adding to database due to string value
+        prices.append(pagePrices[0].text.strip().replace('~','').replace('$','').split('\n')[0])
         if prices[0] == 'free':
             prices[0] == 0.00
-        prices.append(pagePrices[0].text.strip().replace('~','').replace('$','').split('\n')[0])
-        
+            
         links.append(requests.get('http://tinyurl.com/api-create.php?url='+str('https://gg.deals'+shopLinks[0]['href'])).text)
 
     # Return an array of shops and prices
